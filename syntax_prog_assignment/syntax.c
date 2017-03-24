6165563fb2fe7ff2471049b1b187428f60250958
couriersix <johnathan.curry@bison.howard.edu>
2017-03-24 02:04:50 -0400

    fourth commit

--- syntax_prog_assignment/syntax.c
+++ syntax_prog_assignment/syntax.c
@@ -49,3 +49,38 @@ void main() {
 	}
 }
 
+/* lookup - a function to lookup operators and parentheses
+and return the token */
+int lookup(char ch) {
+	switch (ch) {
+	case '(':
+		addChar();
+		nextToken = LEFT_PAREN;
+		break;
+	case ')':
+		addChar();
+		nextToken = RIGHT_PAREN;
+		break;
+	case '+':
+		addChar();
+		nextToken = ADD_OP;
+		break;
+	case '-':
+		addChar();
+		nextToken = SUB_OP;
+		break;
+	case '*':
+		addChar();
+		nextToken = MULT_OP;
+		break;
+	case '/':
+		addChar();
+		nextToken = DIV_OP;
+		break;
+	default:
+		addChar();
+		nextToken = EOF;
+	break;
+	}
+	return nextToken;
+}
